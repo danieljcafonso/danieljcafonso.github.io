d710db85bcd4b877232253213a6a1345f4af369d
Daniel Afonso <daniel.afonso@farfetch.com>
2019-03-22 17:57:17 +0000

    Initial commit

---
+++ precache-manifest.7ceb5f08e8c957bd5c88a05dce494a28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "096070b267daa9fc47c4",
+    "url": "/static/js/main.75133e8d.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "096070b267daa9fc47c4",
+    "url": "/static/css/main.24928d29.chunk.css"
+  },
+  {
+    "revision": "840e0c49ba3ae5d36923c6eec872e8c3",
+    "url": "/index.html"
+  }
+];
